a314f779593e11eac2754c182e003d4e129f3de2
vinceloppeur <vinceloppeur@gmail.com>
2024-07-30 17:11:10 +0700

    fix: add explicit return type to bootstrap function

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,7 +3,7 @@ import { type NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
 
   await app.listen(3000, '0.0.0.0');
